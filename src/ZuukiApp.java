44f2f190f42c1ce9f96a5174bac1cd8711509397
remsysc <raipam44@gmail.com>
2025-08-21 17:47:55 +0800

    added more comments for clarity

--- src/ZuukiApp.java
+++ src/ZuukiApp.java
@@ -3,7 +3,6 @@ public class ZuukiApp {
     public static void main(String[] args) {
         //driver code
 
-        //TODO: CREATE A UI
         zuukiApp();
     }
 
@@ -11,7 +10,6 @@ public class ZuukiApp {
         //TODO: CREATE A FUNCTIONAL UI HERE, THAT THROWS AN EXCEPTION WHENEVER NECESSARRY
 
         createAnimal();
-        addAnimal();
         updateAnimal();
         deleteAnimal();
         displayAnimal();
@@ -20,22 +18,23 @@ public class ZuukiApp {
     public static void createAnimal() {
         //TODO: IMPLEMENT A FUNCTION THAT CREATE AN ANIMAL WITH ATTRIBUTES
         // SUCH NAME, AGE, ETC. REFER TO PDF
-    }
-
-    public static void addAnimal() {
-        //TODO: IMPLEMENT A FUNCTION THAT ADDS AN ANIMAL TO THE INVENTORY
+        // USE PARALLEL ARRAYS like name[i], age[i]
     }
 
     public static void updateAnimal() {
         //TODO: IMPLEMENT A FUNCTION THAT UPDATES ANIMAL DETAILS
+        // same as above
     }
 
     public static void deleteAnimal() {
         //TODO: IMPLEMENT A FUNCTION THAT DELETE AN ANIMAL
+        // either simply marks the animal as "deleted"
+        // or shift them towards the end of an array
     }
 
     public static void displayAnimal() {
         //TODO: IMPLEMENT A FUNCTION THAT DISPLAY AN ANIMAL LIST,
         // THIS DEPENDS IF THE USER PER TYPE , OR ALL OR AGE.
+        // simply loop the arrays
     }
 }
