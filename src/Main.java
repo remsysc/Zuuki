1debf8918c0a4e3500e8781d4f14fd8adb2e1564
remsysc <raipam44@gmail.com>
2025-08-21 17:13:00 +0800

    added a prototype and a lots of todos for group members.

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //driver code
+
+        //TODO: CREATE A UI
+        zuukiApp();
+    }
+
+    public static void zuukiApp() {
+        //TODO: CREATE A FUNCTIONAL UI HERE, THAT THROWS AN EXCEPTION WHENEVER NECESSARRY
+
+        createAnimal();
+        addAnimal();
+        updateAnimal();
+        deleteAnimal();
+        displayAnimal();
+    }
+
+    public static void createAnimal() {
+        //TODO: IMPLEMENT A FUNCTION THAT CREATE AN ANIMAL WITH ATTRIBUTES
+        // SUCH NAME, AGE, ETC. REFER TO PDF
+    }
+
+    public static void addAnimal() {
+        //TODO: IMPLEMENT A FUNCTION THAT ADDS AN ANIMAL TO THE INVENTORY
+    }
+
+    public static void updateAnimal() {
+        //TODO: IMPLEMENT A FUNCTION THAT UPDATES ANIMAL DETAILS
+    }
+
+    public static void deleteAnimal() {
+        //TODO: IMPLEMENT A FUNCTION THAT DELETE AN ANIMAL
+    }
+
+    public static void displayAnimal() {
+        //TODO: IMPLEMENT A FUNCTION THAT DISPLAY AN ANIMAL LIST,
+        // THIS DEPENDS IF THE USER PER TYPE , OR ALL OR AGE.
+    }
+}
