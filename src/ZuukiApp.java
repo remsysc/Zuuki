32b21ef2058771f1f0a183dc2cc5a90db0e135c9
remsysc <raipam44@gmail.com>
2025-08-24 18:45:40 +0800

    added updateAnimal, deleteAnimal, and some display functions

--- src/ZuukiApp.java
+++ src/ZuukiApp.java
@@ -30,8 +30,9 @@ public class ZuukiApp {
             counter,
             kSize
         ); // PASS BY VALUE of the REFERENCE
-        updateAnimal();
-        deleteAnimal();
+        //  updateAnimal(name, age, species, enclosure_number, counter, kSize);
+        //deleteAnimal(name, age, species, enclosure_number, counter, kSize);
+        System.out.println(counter);
         displayAnimal(name, age, species, enclosure_number, counter);
     }
 
@@ -73,19 +74,80 @@ public class ZuukiApp {
             }
         } while (!isCleanInputs);
 
-        scan.close(); //close the scanner
+        //scan.close(); //close the scanner
         return counter;
     }
 
-    public static void updateAnimal() {
-        //TODO: IMPLEMENT A FUNCTION THAT UPDATES ANIMAL DETAILS
-        // same as above
+    public static void updateAnimal(
+        String[] name,
+        int[] age,
+        String[] species,
+        int[] enclosure_number,
+        int counter,
+        int kSize
+    ) {
+        //Ask the index of availabe animals
+
+        Scanner scan = new Scanner(System.in);
+        boolean isValid = false;
+        do {
+            System.out.println("Available index 0 - " + (counter - 1));
+            System.out.println("What animal you want to update?");
+
+            try {
+                int userInput = Integer.parseInt(scan.nextLine());
+
+                // checks if input is valid
+                if (userInput > counter - 1) {
+                    System.out.println("Invalid Range.");
+                    continue;
+                } else createAnimal(
+                    name,
+                    age,
+                    species,
+                    enclosure_number,
+                    userInput,
+                    kSize
+                );
+
+                isValid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input: " + e.getMessage());
+            }
+        } while (!isValid);
     }
 
-    public static void deleteAnimal() {
-        //TODO: IMPLEMENT A FUNCTION THAT DELETE AN ANIMAL
-        // either simply marks the animal as "deleted"
-        // or shift them towards the end of an array
+    public static void deleteAnimal(
+        String[] name,
+        int[] age,
+        String[] species,
+        int[] enclosure_number,
+        int counter,
+        int kSizes
+    ) {
+        //TODO: SHIFT THE "DELETED" to the end of array
+
+        Scanner scan = new Scanner(System.in);
+        boolean isValid = false;
+        do {
+            try {
+                System.out.print("Inde of animal you want to delete: ");
+                int userInput = Integer.parseInt(scan.nextLine());
+
+                //checks the validity
+                if (userInput > counter - 1) {
+                    System.out.println("Invalid Index");
+                } else {
+                    name[userInput] = "[N/A]";
+                    age[userInput] = 0;
+                    species[userInput] = "[N/A]";
+                    enclosure_number[userInput] = 0;
+                }
+                isValid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input");
+            }
+        } while (!isValid);
     }
 
     public static void displayAnimal(
@@ -96,20 +158,92 @@ public class ZuukiApp {
         int counter
     ) {
         //TODO: IMPLEMENT A FUNCTION THAT DISPLAY AN ANIMAL LIST,
-        // THIS DEPENDS IF THE USER PER TYPE , OR ALL OR AGE.
-        // simply loop the arrays
+        //search animal by name, count how many animals belogn to specific species
+        // and display all animals housed in a specific enclosure
+        //
+        //display options
         //
 
+        int count = countAnimals(10, enclosure_number, counter);
+
+        // searchAnimalByName(
+        //        "rem",
+        //        name,
+        //        age,
+        //        species,
+        //        enclosure_number,
+        //        counter
+        //    );
+    }
+
+    public static void displayAnimalsByEnclosureNumber(
+        int userInput,
+        String[] name,
+        int[] age,
+        String[] species,
+        int[] enclosure_number,
+        int counter
+    ) {}
+
+    //method overloading
+    public static int countAnimals(
+        String userInput,
+        String[] species,
+        int counter
+    ) {
+        int total = 0;
+
         for (int i = 0; i < counter; i++) {
-            System.out.println(
-                name[i] +
-                " " +
-                age[i] +
-                " " +
-                species[i] +
-                " " +
-                enclosure_number[i]
-            );
+            if (userInput.equalsIgnoreCase(species[i])) total++;
         }
+
+        return total;
+    }
+
+    public static int countAnimals(
+        int userInput,
+        int[] enclosure_number,
+        int counter
+    ) {
+        int total = 0;
+
+        for (int i = 0; i < counter; i++) {
+            if (enclosure_number[i] == userInput) total++;
+        }
+
+        return total;
+    }
+
+    public static void searchAnimalByName(
+        String userInput,
+        String[] name,
+        int[] age,
+        String[] species,
+        int[] enclosure_number,
+        int counter
+    ) {
+        int index = isNameValid(userInput, name, counter);
+
+        if (index < 0) return;
+
+        //display all information regarding the found userInput
+
+        System.out.println("Name: " + name[index]);
+        System.out.println("Age: " + age[index]);
+        System.out.println("Species: " + species[index]);
+        System.out.println("Enclosure Number: " + enclosure_number[index]);
+    }
+
+    public static int isNameValid(
+        String userInput,
+        String[] name,
+        int counter
+    ) {
+        for (int i = 0; i < counter; i++) {
+            //linear search
+            if (userInput.equalsIgnoreCase(name[i])) return i;
+        }
+        System.out.println(userInput + " not found.");
+        return -1;
     }
 }
